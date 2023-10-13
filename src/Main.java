c0018ab1c4874aca7dbf6f8a05eef86a3bdfda43
olga-kri <krivenkoolga1987@gmail.com>
2023-10-13 19:32:59 +0500

    Homework

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Задача 1: объявить три строки для хранения ФИО сотрудника, вывести в консоль
+
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
@@ -11,10 +12,12 @@ public class Main {
         System.out.println("Ф.И.О. сотрудника - " + fullName);
 
         // Задача 2:Напишите программу, которая изменит написание ФИО на полностью заглавные буквы.
+
         String fullNameToUpperCase = fullName.toUpperCase(Locale.ROOT);
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " +fullNameToUpperCase);
 
         // Задача 3:Напишите программу, которая заменяет символ «ё» на символ «е».
+
         String fullName1 = "Иванов Семён Семёнович";
         String fullNameCorrect = fullName1.replace('ё', 'е');
         System.out.println("Ф.И.О. сотрудника - " + fullNameCorrect);
